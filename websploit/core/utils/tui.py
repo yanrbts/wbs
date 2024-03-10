9c85c81cbbd5f4fc6bf2c21219a9de45e1752a7c
yanrbts <772166784@qq.com>
2024-03-10 22:59:54 +0800

    modify tui file

--- websploit/core/utils/tui.py
+++ websploit/core/utils/tui.py
@@ -5,11 +5,11 @@ def logo():
 
     return f"""\033[92m
     ____    __    ____
-    \   \  /  \  /   /     |   \033[95m Welcome to Websploit \033[92m
+    \   \  /  \  /   /     |   \033[95m Welcome to wbs \033[92m
      \   \/    \/   /      |   \033[95m Version : {version} \033[92m
-      \            /       |   \033[95m https://github.com/websploit/websploit \033[92m
-       \    /\    /        |   \033[95m Author : Fardin Allahverdinazhand \033[92m
-        \__/  \__/         |   \033[95m Codename : Reborn \033[92m
+      \            /       |   \033[95m https://github.com/yanrbts/wbs.git \033[92m
+       \    /\    /        |   \033[95m Author : yanrbts \033[92m
+        \__/  \__/         |   \033[95m Codename : wbs \033[92m
 
     \033[0m
     """
